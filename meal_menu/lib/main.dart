a18cc55026b17ce9fe2cdc9221d42885c0e79eb1
Vicky rana <53395548+vickyrana2401@users.noreply.github.com>
2024-04-28 19:54:13 +0530

    Update main.dart

--- meal_menu/lib/main.dart
+++ meal_menu/lib/main.dart
@@ -8,7 +8,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My first App'),
+        title: Text('Practice'),
         centerTitle: true,
         backgroundColor: Colors.red[800],
       ),
