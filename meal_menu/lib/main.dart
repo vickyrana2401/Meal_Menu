eda2da757debc40cc1386a43d427076e2ce40546
vickyrana2401 <vickyrana2429@gmail.com>
2024-04-28 19:31:34 +0530

    Initial commit

---
+++ meal_menu/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[800],
+      ),
+      body: Center(
+        child: Text(
+          'Hello Rana \n',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[800],
+              fontFamily: 'd'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('+'),
+        backgroundColor: Colors.red[800],
+      ),
+    );
+  }
+}
